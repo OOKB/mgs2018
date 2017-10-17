fbae43bc12f044a74e73e441faca05dd705245a0
trevorsee <tcarr@mica.edu>
2017-10-17 11:11:57 -0500

    strip down splash page

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import Page from './Page'
+import PropTypes from 'prop-types'
+import mgsBlock from '../mgs2017LogoBlock.svg'
+import Blurb from './Blurb/Blurb'
 
-function Splash() {
+function Splash({ siteName }) {
   return (
-    <Page className="App">
+    <div>
+      <Blurb mgsBlock={mgsBlock} siteName={siteName} />
       <h2>2018</h2>
-    </Page>
+    </div>
   )
 }
+
+Splash.propTypes = {
+  siteName: PropTypes.string,
+}
+Splash.defaultProps = {
+  siteName: 'MICA Grad Show',
+}
+
 export default Splash
