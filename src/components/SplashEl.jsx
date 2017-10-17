68bf081d50e5a683d9c1807393196912cfe0a187
Kai Curry <kai@cape.io>
2017-10-17 16:25:55 -0400

    display entire menu instead

--- src/components/SplashEl.jsx
+++ src/components/SplashEl.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import LinkEl from 'cape-mixer/lib/Link/Link'
+// import LinkEl from 'cape-mixer/lib/Link/Link'
 import mgsBlock from '../mgs2017LogoBlock.svg'
 import Blurb from './Blurb/Blurb'
+import Menu from './Header/Menu'
 
-function Splash({ login, loginAction, siteName }) {
+function Splash({ siteName }) {
   return (
     <div>
       <Blurb mgsBlock={mgsBlock} siteName={siteName} />
-      <h2>2018</h2>
-      <LinkEl action={loginAction} {...login} />
+      <Menu />
+      <h2>{siteName}</h2>
+      {/* <LinkEl action={loginAction} {...login} /> */}
     </div>
   )
 }
 
 Splash.propTypes = {
-  login: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-  loginAction: PropTypes.func.isRequired,
+  // login: PropTypes.shape({
+  //   id: PropTypes.string.isRequired,
+  //   name: PropTypes.string.isRequired,
+  // }).isRequired,
+  // loginAction: PropTypes.func.isRequired,
   siteName: PropTypes.string.isRequired,
 }
 Splash.defaultProps = {
