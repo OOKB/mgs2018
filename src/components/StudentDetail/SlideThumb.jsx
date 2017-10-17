75166b6c4adc1cb0263355cd8a612c83f772946d
Kai Curry <kai@cape.io>
2017-10-17 16:49:33 -0400

    alt or title. fix a couple eslint things

--- src/components/StudentDetail/SlideThumb.jsx
+++ src/components/StudentDetail/SlideThumb.jsx
@@ -4,22 +4,28 @@ import classnames from 'classnames'
 
 import Video from './Video'
 
-function SlideThumb({ classNames, src, title, handleClick, videoInfo }) {
+function SlideThumb({ alt, classNames, src, title, handleClick, videoInfo }) {
   return (
     <li className={classnames(classNames)}>
       { videoInfo && videoInfo.url ? <Video {...videoInfo} /> : (
-        <img src={`${src}?w=1200`} title={title} alt={title} onClick={handleClick} />
+        <img src={`${src}?w=1200`} title={title} alt={alt || title} onClick={handleClick} />
       )}
     </li>
   )
 }
 
 SlideThumb.propTypes = {
-  src: PropTypes.string,
-  title: PropTypes.string,
+  alt: PropTypes.string,
   classNames: PropTypes.object,
   handleClick: PropTypes.func,
+  src: PropTypes.string,
+  title: PropTypes.string,
   videoInfo: PropTypes.object,
 }
-
+SlideThumb.defaultProps = {
+  alt: null,
+  handleClick: null,
+  src: null,
+  title: null,
+}
 export default SlideThumb
