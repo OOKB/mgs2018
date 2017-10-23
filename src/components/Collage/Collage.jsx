a7e77dc718fa906df01ca6c5212c914ed0e3e348
trevorsee <tcarr@mica.edu>
2017-10-23 04:58:37 -0500

    moving loop working now

--- src/components/Collage/Collage.jsx
+++ src/components/Collage/Collage.jsx
@@ -1,39 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { map, random } from 'lodash'
+import { map } from 'lodash'
 import { Wrapper } from './styles'
 import ImageContainer from './ImageContainer'
 
-const Collage = ({ collection }) => {
-
-  let getPos = () => {
-    return {
-      current: {
-        x: random(0,50),
-        y: random(0,50)
-      },
-      to: {
-        x: random(50,400),
-        y: random(50,400)
-      }
+class Collage extends React.Component {
+  constructor(props) {
+    	super(props);
+        this.state = {
+        };
     }
+
+  render() {
+    let test = () => { console.log( 'testing function' ) }
+    return (
+      <Wrapper>
+        {this.props.collection && map( this.props.collection, (item, i) =>
+          <ImageContainer key={item.id} item={item} pos={i} func={test.bind(this)} />
+        )}
+      </Wrapper>
+    )
   }
-
-  return (
-    <Wrapper>
-      {collection && map( collection, item =>
-        <ImageContainer item={item} position={getPos()} />
-      )}
-    </Wrapper>
-  )
-}
-
-Collage.propTypes = {
-  collection: PropTypes.array.isRequired
-}
-
-Collage.defaultProps = {
-  collection: null
 }
 
 export default Collage
--- src/components/Collage/ImageContainer.jsx
+++ src/components/Collage/ImageContainer.jsx
@@ -1,27 +1,74 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Motion, spring } from 'react-motion';
+import MeasureIt from 'react-measure-it'
+import { map, random } from 'lodash'
 import { Image } from './styles'
 
-const ImageContainer = ({ item, position, test }) => {
-  console.log(position)
-  return (
-    <Motion
-      defaultStyle={{ left: position.current.x, top: position.current.y }}
-      style={{ left: spring( position.to.x ), top: spring( position.to.y ) }}
-      onRest={ () => { console.log('test') }}>
+class ImageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pos: null,
+      stiffness: 10,
+      damping: 20,
+      currentX: 0,
+      currentY: 0,
+      toX: 0,
+      toY: 0
+    };
+  }
 
-      { style =>
-      <Image key={item.id} src={`${item.image.url}?w=100`} style={style}></Image>
-      }
-    </Motion>
-  )
+  initialPos() {
+    console.log(this.props.pos)
+
+    return {
+      stiffness: random(10,20),
+      damping: random(10,40),
+      currentX: random(0,50),
+      currentY: random(0,50),
+      toX: random(50,400),
+      toY: random(50,400)
+    }
+  }
+
+  updatePos() {
+    let newState = {
+      stiffness: random(10,20),
+      damping: random(10,40),
+      currentX: random(0,50),
+      currentY: random(0,50),
+      toX: random(50,400),
+      toY: random(50,400)
+    }
+    // https://github.com/chenglou/react-motion/issues/322
+    // issue with react-motion changing state onRest
+    window.setTimeout(() => {
+      this.setState(newState)
+    }, 0);
+  }
+
+  componentWillMount() {
+    this.setState(this.initialPos())
+  }
+
+  render() {
+
+    console.log(this.props.containerWidth)
+
+    return (
+      <Motion
+        key={this.props.item.id}
+        defaultStyle={{ left: this.state.currentX, top: this.state.currentY }}
+        style={{ left: spring( this.state.toX, {stiffness: this.state.stiffness, damping: this.state.damping} ), top: spring( this.state.toY, {stiffness: this.state.stiffness, damping: this.state.damping}) }}
+        onRest={ () => { this.updatePos() }}>
+
+        { style =>
+        <Image src={`${this.props.item.image.url}?w=100`} style={style}></Image>
+        }
+      </Motion>
+    )
+  }
 }
 
-ImageContainer.propTypes = {
-}
-
-ImageContainer.defaultProps = {
-}
-
-export default ImageContainer
+export default MeasureIt()(ImageContainer)
