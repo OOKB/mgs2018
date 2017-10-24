6e7a74a8f01ff46febf1e4a25a81d04029941433
Kai Curry <kai@cape.io>
2017-10-24 11:51:41 -0400

    update art script to dl height/width

--- src/select/sliderArt.js
+++ src/select/sliderArt.js
@@ -1,19 +1,20 @@
 import { writeFileSync } from 'fs'
-import { flow, mapValues, pick, pickBy, has } from 'lodash/fp'
+import { flow, keys, mapValues, overEvery, pick, pickBy, has } from 'lodash/fp'
+import { get } from 'axios'
 import { buildFullEntity, selectGraph } from 'redux-graph'
 import { replaceField, setWith } from 'cape-lodash'
 import data from '../../mica2017.json'
 import { getArtItems } from './art'
 
 const buildPerson = pick(['name', 'id'])
-const buildImage = pick(['url', 'id'])
+const buildImage = pick(['url', 'id', ''])
 const buildArtItem = flow(
   replaceField('image', buildImage),
   setWith('student', 'agent', buildPerson),
   pick(['student', 'image', 'id', 'title'])
 )
 
-function rmUsers({ student: { id } }) {
+function notTestUser({ student: { id } }) {
   return id !== 'Y6dg66IQmUcHS13nvN2cGb6rVVD3' && id !== 'iB0TJ517wnM3NzkVmcIAmLvxy112'
 }
 
@@ -21,8 +22,25 @@ function getArt(state) {
   return flow(
     getArtItems,
     mapValues(flow(buildFullEntity(1, selectGraph(state)), buildArtItem)),
-    pickBy(has('image.url'))
+    pickBy(overEvery([has('image.url'), notTestUser]))
   )(state)
 }
 
-writeFileSync('/tmp/art.json', JSON.stringify(getArt(data)))
+const art = getArt(data)
+const artIds = keys(art)
+const maxId = artIds.length - 1
+console.log('max', maxId)
+function getSize(index = 0) {
+  const url = art[artIds[index]].image.url
+  function handleRes({ data: { PixelHeight, PixelWidth } }) {
+    art[artIds[index]].image.height = PixelHeight
+    art[artIds[index]].image.width = PixelWidth
+    console.log(index)
+    if (index === maxId) {
+      return writeFileSync('/tmp/art.json', JSON.stringify(art))
+    }
+    return getSize(index + 1)
+  }
+  get(`${url}?fm=json`).then(handleRes)
+}
+getSize()
