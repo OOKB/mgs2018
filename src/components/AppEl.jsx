1af7a9dcf1ac0c004e79c8ab58911428db336e16
trevorsee <tcarr@mica.edu>
2017-11-29 23:21:34 -0600

    init homepage branch

--- src/components/AppEl.jsx
+++ src/components/AppEl.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 // import css from 'cape-style'
 import './App.css'
 import Page from './Page'
+import Collage from './Collage/Collage'
 import Schedule from './Schedule/Schedule'
 
 function AppEl() {
   return (
     <Page className="App">
+      <Collage />
       <Schedule />
     </Page>
   )
