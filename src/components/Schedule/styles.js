c17874162b0c73ed44f9ffa1df870fb5d39d92f1
trevorsee <tcarr@mica.edu>
2017-12-08 21:35:52 -0600

    styling schedule

--- src/components/Schedule/styles.js
+++ src/components/Schedule/styles.js
@@ -6,12 +6,14 @@ export const GroupHeader = styled.h2`
   font-weight: bold;
   text-transform: uppercase;
   padding: 2rem 0;
-  border-top: 1px solid ${colors.grey};
-  border-bottom: 1px solid ${colors.grey};
+  border-top: 1px solid ${props => props.theme.color};
+  border-bottom: 1px solid ${props => props.theme.color};
   letter-spacing: 0.02em;
   margin: 0;
   margin-bottom: 2rem;
   @media (min-width: ${sizes.medium}) {
+    border-top: 1px solid ${colors.grey};
+    border-bottom: 1px solid ${colors.grey};
   }
 `
 
@@ -24,27 +26,43 @@ export const Group = styled.section`
 `
 export const ShowItem = styled.section`
   padding: 0 !important;
-  padding-right: 4rem !important;
-  width: calc(100% / 3);
+  width: calc(100% / 1);
   margin: 0 0 5rem;
   background: none;
   z-index: 1;
   font-size: 1.25rem;
-  &:nth-child(3) {
-    padding-right: 0 !important;
-  }
   &:hover {
     background: none;
     color: ${props => props.theme.color};
   }
   h1 { font-size: 2.25rem; }
   h2 { font-size: 1.25rem; }
+  @media (min-width: ${sizes.medium}) {
+    width: calc(100% / 2);
+    padding-right: 4rem !important;
+    &:nth-child(2n) {
+      padding-right: 0 !important;
+    }
+  }
+  @media (min-width: ${sizes.large}) {
+    width: calc(100% / 3);
+    padding-right: 4rem !important;
+    &:nth-child(2n) {
+      padding-right: 4rem !important;
+    }
+    &:nth-child(3n) {
+      padding-right: 0 !important;
+    }
+  }
 `
 
 export const Blurb = styled.p`
-  width: calc(100% / 3);
+  width: 100%;
   margin: 0 0 5rem;
-  padding-right: 4rem;
   font-size: 1.25rem;
   line-height: 1.3;
+  @media (min-width: ${sizes.large}) {
+    width: calc(100% / 3);
+    padding-right: 4rem;
+  }
 `
